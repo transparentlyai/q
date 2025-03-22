ccccc73276112dee7c1ecd1748db91a9150b6377
transparentlyai <transparentlyai@users.noreply.github.com>
2025-03-23 01:37:33 +0800

    Bump version to 0.5.1
    - Update version number in __init__.py, setup.py, and pyproject.toml

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="q",
-    version="0.5.0",
+    version="0.5.1",
     description="A command Line AI Assistant",
     author="mauro@sauco.net",
     author_email="mauro@sauco.net",
